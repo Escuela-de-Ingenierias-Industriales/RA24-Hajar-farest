b5ff3e11d29aaaf6f8d9ed7f274d708644e81159
Hajar <Hajar@DESKTOP-CD7VI8R.wifi.local>
2024-12-29 15:49:09 +0100

    Cinematica S ok

--- Hito33.m
+++ Hito33.m
@@ -1,4 +1,4 @@
-Fun_Drch=tf(0.059501967557249, [1,29.247647205276063])
-Fun_Izq=tf(0.055196645073388, [1,26.948507496407224])
-[C_Izq,infoIzq]=pidtune(Fun_Izq,'PI')
-C_TfIzq=tf(C_Izq)
+Fun_Drch=tf(0.058162404004943, [1,28.392163768618786])
+Fun_Izq=tf(0.054272383069799, [1,27.017926457758794])
+[C_Drch,infoDrch]=pidtune(Fun_Drch,'PI')
+C_TfDrch=tf(C_Drch)
